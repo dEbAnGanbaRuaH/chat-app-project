3c2b2d6f40f97c865abce1aca15c4fec47f2ac68
dEbAnGanbaRuaH <debangan.baruah@gmail.com>
2022-03-24 19:09:57 +0530

    first commit

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKSaUhD5DhxUAd8Hnno6ZvE7OWuvUMzGE",
+  authDomain: "chat-app-ee699.firebaseapp.com",
+  projectId: "chat-app-ee699",
+  storageBucket: "chat-app-ee699.appspot.com",
+  messagingSenderId: "1045412902486",
+  appId: "1:1045412902486:web:a5cd48095cf04b00c96df7",
+  measurementId: "G-0T5Z3Z7VEC"
+};
